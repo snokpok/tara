a0c01314fe417095a3f811c47c4a2ac65a013ae8
Anna Hsu <adhsu@usc.edu>
2023-04-22 12:12:37 -0700

    push whatever i have rn lol

---
+++ apps/admin/pages/classes.tsx
@@ -0,0 +1,27 @@
+
+
+export function Classes() {
+	/*
+	 * Replace the elements below with your own.
+	 *
+	 * Note: The corresponding styles are in the ./index.css file.
+	 */
+	return (
+		<div>
+      <div className="wrapper">
+        <div className="container">
+          <div id="classes">
+            <h1>Your Courses</h1>
+          </div>
+          <div id="hero" className="rounded">
+						<div className="text-container">
+							<h2>Spring 2023</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+		</div>
+	);
+}
+
+export default Classes;
