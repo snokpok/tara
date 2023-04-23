eb33f246be49d401169fef2dd6a4608c81a3196c
Anna Hsu <adhsu@usc.edu>
2023-04-22 17:41:41 -0700

    course cards and flex

--- apps/admin/components/coursecard.tsx
+++ apps/admin/components/coursecard.tsx
@@ -1,12 +1,28 @@
-import { Badge, Card, Text, Link } from '@nextui-org/react';
+import { Button, Card, Container, Text } from '@nextui-org/react';
 
 const CourseCard = () => {
 	return (
-		<Card isHoverable variant="shadow">
+		<Card variant="shadow" css={{ borderColor: '#fff', padding: '0px' }}>
 			<Card.Body>
-				<Text>HW 6: Hash Tables</Text>
-				<Text>The date</Text>
-				<Badge>assignment type</Badge>
+				<Container
+					display="flex"
+					direction="row"
+					wrap="nowrap"
+					justify="space-between"
+					alignItems="center"
+					css={{ padding: '0px' }}
+				>
+					<Container display="flex" direction="column">
+						<Text b>HW 6: Hash Tables</Text>
+						<Text size="$xs" i>
+							March 23, 2023
+						</Text>
+					</Container>
+					<Container display="flex" justify="flex-end" css={{ gap: '8px' }}>
+						<Button>Edit assignment</Button>
+						<Button>View analytics</Button>
+					</Container>
+				</Container>
 			</Card.Body>
 		</Card>
 	);
